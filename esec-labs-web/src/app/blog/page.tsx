da26f82cde4e1773743ea1a29494dcc719e36434
Isaiah6225 <isaiahjohnson6225@gmail.com>
2025-07-31 23:49:41 -0400

    Added search bar on blog page.

--- esec-labs-web/src/app/blog/page.tsx
+++ esec-labs-web/src/app/blog/page.tsx
@@ -5,7 +5,25 @@ export default function Blog() {
   return (
     <div>
       <Container>
+          <div className="flex flex-col items-center text-center space-y-32">
 
+		  {/* Title and filter nav bar */}
+		  <section className="pt-24">
+		    <h1 className="text-5xl mb-4 font-bold text-black">
+			Learn new skils
+		    </h1>
+		    <div className="justify-center">	
+			    <nav className="font-bold absolute flex ">
+				<input 
+					className="w-128 bg-white text-black shadow-md rounded-lg p-4 border border-gray-200"
+					type="text" 
+					placeholder="Search for blogs..."
+
+				/>
+			    </nav>
+		    </div>
+		  </section>
+	  </div>
       </Container>
     </div>
   );
